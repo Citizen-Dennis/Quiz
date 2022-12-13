828754891a01453100b58d0d9100b668f52947ba
Den <appahdennisamoakoadb@gmail.com>
2022-12-13 16:09:28 +0000

    Addede javascript code

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
- 
+//variable to hold selected answer
+let ans = ['Accra', 'Torvalds'];
+
+let check = function(){
+    let sval1 = document.querySelector("input[name=capital]:checked").value;
+if(sval1 == ans[0])
+    alert('Correct')
+else{
+    alert('Wrong');
+    answers.innerHTML = 'Accra\n';
+}
+
+let sval2 = document.querySelector("input[name=git]:checked").value;
+if(sval2 == ans[1])
+    alert('Correct')
+else{
+    alert('Wrong');
+    answers.innerHTML = 'Linux Torvald';
+}
+} 
